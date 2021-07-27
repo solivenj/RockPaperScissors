65b738ba0ff48de0538bf5d12d9c9d76fb6701c6
solivenj <soliven.j@husky.neu.edu>
2021-07-27 19:46:02 -0400

    Basic UI

--- app.js
+++ app.js
@@ -3,40 +3,72 @@ function computerPlay() {
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function getWinner(playerSelection, computerSelection) {
     let pSelection = playerSelection.toLowerCase();
     let cSelection = computerSelection.toLowerCase();
 
     if (pSelection == cSelection) {
-        return "Tie!";
+        return "tie";
     }
     
     if ((pSelection == "paper" && cSelection == "rock")  || (pSelection == "rock" && cSelection == "scissors") || (pSelection == "scissors" && cSelection == "paper")) {
-        return "You win";
+        return "player"
     } else {
-        return "You lose!";
+        return "computer";
     }
 }
 
-function game() {
-    let pScore = cScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let pMove = prompt("Enter a move (Rock, Paper, or Scissors): ");
-        let cMove = computerPlay()
-        let result = playRound(pMove, cMove);
-        console.log(`Player: ${pMove}\tComputer: ${cMove}`);
-        console.log(`${result}`);
-        if (result[4] == 'w') {
-            pScore++;
-        } else if (result[4] == 'l') {
-            cScore++;
-        }
-        console.log(`Player Score: ${pScore}\tComputer Score: ${cScore}`);
+function updateScore(winner) {
+    if (winner == "player") {
+        pScore++;
+    } else if (winner == "computer") {
+        cScore++;
+    }
 
+    pScoreDiv.textContent = `Player Score: ${pScore}`;
+    cScoreDiv.textContent = `Computer Score: ${cScore}`;
+}
+
+function addReset() {
+    let btn = document.createElement("button");
+    btn.textContent = "Play Again";
+    winner.appendChild(btn);
+    btn.addEventListener('click', reset);
+}
+
+function checkWinner() {
+    if (pScore == 5 || cScore == 5) {
+        winner.textContent = "Winner: ";
+        winner.textContent += pScore == 5 ? "Player" : "Computer";
+        buttons.forEach(button => button.disabled = true);
+        addReset();
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function playRound(e) {
+    let pMove = this.textContent;
+    let cMove = computerPlay();
+    moves.textContent = `Player Selection: ${pMove}\tComputer Selection: ${cMove}`;
+    let winner = getWinner(this.textContent, computerPlay());
+    updateScore(winner);
+    checkWinner();
+}
+
+function reset(e) {
+    pScore = cScore = 0;
+    winner.textContent = "";
+    moves.textContent = "Choose a move:";
+    pScoreDiv.textContent = "";
+    cScoreDiv.textContent = "";
+    buttons.forEach(button => button.disabled = false);
+}
+
+let pScore = cScore = 0;
+const pScoreDiv = document.getElementById("pScore");
+const cScoreDiv = document.getElementById("cScore");
+const winner = document.getElementById("winner");
+const moves = document.getElementById("moves");
+moves.textContent = "Choose a move:";
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
 
