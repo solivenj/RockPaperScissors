90883e7eb190518eb2ac8e28645bde591c3da244
solivenj <soliven.j@husky.neu.edu>
2021-07-28 16:28:41 -0400

    Updated UI

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    let moves = ['Rock', 'Paper', 'Scissors'];
+    let moves = ['rock', 'paper', 'scissors'];
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
@@ -18,15 +18,25 @@ function getWinner(playerSelection, computerSelection) {
     }
 }
 
+function updateResult(str) {
+    result.textContent = str;
+}
+
 function updateScore(winner) {
     if (winner == "player") {
         pScore++;
+        updateResult("Player wins!");
     } else if (winner == "computer") {
         cScore++;
+        updateResult("Computer wins!");
+    } else {
+        updateResult("Tie!");
     }
 
-    pScoreDiv.textContent = `Player Score: ${pScore}`;
-    cScoreDiv.textContent = `Computer Score: ${cScore}`;
+    pScoreDiv.textContent = `${pScore}`;
+    cScoreDiv.textContent = `${cScore}`;
+
+    checkWinner();
 }
 
 function addReset() {
@@ -45,22 +55,36 @@ function checkWinner() {
     }
 }
 
+function updatePlayerCPUImage(imgID, move) {
+    let img = document.getElementById(imgID);
+    img.style.display = "block";
+    if (move == "paper") {
+        img.src = "images/paper-white.png";
+    } else if (move == "rock") {
+        img.src = "images/rock-white.png";
+    } else {
+        img.src = "images/scissors-white.png";
+    }
+}
+
 function playRound(e) {
-    let pMove = this.textContent;
-    let cMove = computerPlay();
-    moves.textContent = `Player Selection: ${pMove}\tComputer Selection: ${cMove}`;
-    let winner = getWinner(this.textContent, computerPlay());
+    let pMove = this.textContent.toLowerCase();
+    let cMove = computerPlay().toLowerCase();
+    updatePlayerCPUImage("pMove", pMove);
+    updatePlayerCPUImage("cMove", cMove);
+    let winner = getWinner(pMove, cMove);
     updateScore(winner);
-    checkWinner();
 }
 
 function reset(e) {
     pScore = cScore = 0;
     winner.textContent = "";
-    moves.textContent = "Choose a move:";
-    pScoreDiv.textContent = "";
-    cScoreDiv.textContent = "";
+    pScoreDiv.textContent = "0";
+    cScoreDiv.textContent = "0";
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach(img => img.style.display = 'none');
     buttons.forEach(button => button.disabled = false);
+    result.textContent = "";
 }
 
 let pScore = cScore = 0;
@@ -68,7 +92,9 @@ const pScoreDiv = document.getElementById("pScore");
 const cScoreDiv = document.getElementById("cScore");
 const winner = document.getElementById("winner");
 const moves = document.getElementById("moves");
-moves.textContent = "Choose a move:";
+const prompt = document.getElementById("prompt");
+let result = document.getElementById("result");
+prompt.textContent = "Choose a move:";
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
 
